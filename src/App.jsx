1ab020530d235e712ca0f493534a49c226e1a798
sayedalali1 <sayedalali1@gmail.com>
2024-08-04 20:18:56 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 // src/App.jsx
+import './App.css';
 
 const App = () => {
+  const plans = [
+    { task: 'Wash car', completed: true },
+    { task: 'Go shopping', completed: false },
+    { task: 'Iron clothes', completed: true },
+  ];
 
   return (
-    <h1>Hello world!</h1>
+    <>
+      <h1>Task List</h1>
+      <ul>
+        {plans.map((plan, index) => (
+          <li key={index} className={plan.completed ? 'completed' : 'not-completed'}>
+            {plan.task}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
-export default App
+export default App;
